b008eee17c59bdcb27562c0498407cf6819b4345
angiegonza125 <69263578+angiegonza125@users.noreply.github.com>
2020-11-05 12:00:06 -0600

    js

---
+++ js/menu.js
@@ -0,0 +1,36 @@
+//variables
+const menu = document.querySelector('#movil');
+const degradado = document.querySelector('#degradado');
+const boton = document.querySelector('#menu');
+const salir = document.querySelector('#salir');
+const enlaces = document.querySelector('.link');
+
+//eventos
+boton.addEventListener('click', mostrarmenu);
+salir.addEventListener('click', ocultarmenu);
+document.addEventListener('keydown', filtrotecla);
+degradado.addEventListener('click', ocultarmenu);
+enlaces.forEach(enlace=>{
+    enlace.addEventListener('click', ocultarmenu);
+})
+
+//funciones
+function filtrotecla(e){
+    e.keyCode === 27 && ocultarmenu();
+}
+
+function mostrarmenu(){
+    menu.style.right = '0';
+    degradado.style.display = "block";
+    setTimeout(()=>{
+        degradado.style.background = 'rgba(0, 0, 0, .5)';
+    }, 10);
+}
+
+function ocultarmenu(){
+    menu.style.right = '-80%';
+    degradado.style.background = 'rgba(0, 0, 0, 0)';
+    setTimeout(()=>{
+        degradado.style.display = 'none';
+    }, 250);
+}
